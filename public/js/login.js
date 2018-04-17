44717511dbbd33b362d498e3e29234ad1dc79e8b
Keith Kovach <kovachks90@gmail.com>
2018-04-16 21:32:26 -0400

    switched out signup toggling with a modal instead

--- public/js/login.js
+++ public/js/login.js
@@ -1,38 +1,39 @@
-if (localStorage.getItem("uid=")) {
-
-    var data = {
-        uid: localStorage.getItem("uid=")
-    }
-    console.log("this is a test fire for local storages")
-    $.ajax({
-        method: "POST",
-        url: "/authenticate",
-        data: localStorage.getItem("uid=")
-    })
-    .then(function(data) {
-        if (data.email) {
-            $("#signedIn").show().text("Signed in as " + data.email)
-            $("#loginDiv").hide()
-            $("#signupButton").hide()
-            $("#logout").show()
-            localStorage.setItem('uid=', data.uid)
-        }
-        //Invalid responding with the data provided from the server which is an alert.  Switch to modal in future
-        else {
-            alert(data)
-        }
-    })
-}
-
-//Displaying input fields for signing up
-$(document).on('click', "#signupButton", function() {
-    $("#loginButton").show()
-    $("#loginDiv").hide()
-    $("#signupDiv").show()
-    $("#signupButton").hide()
-    $("#searchDiv").hide()
-    $("#newTrip").show()
+$(document).ready(function() {
+    $(document).on("click", "#signupButton", function() {
+        $(".modal-content").html(
+            "<div class='modal-header'><h4>Singup</h4><button type='button' class='close'" +
+             "data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>" + 
+             "<div class='modal-body'><h5>Email</h5>" + 
+             "<input placeholder='Email' type='text' id='email'><h5>Password</h5><input placeholder='Password' type='password' id='password'>" + 
+             "</div><div class='modal-footer'><button id='signup'>Submit</button><button id='cancel'>Cancel</button></div>"
+        )
+    })    
 })
+
+// if (localStorage.getItem("uid=")) {
+//     var data = {
+//         uid: localStorage.getItem("uid=")
+//     }
+//     console.log("this is a test fire for local storages")
+//     $.ajax({
+//         method: "POST",
+//         url: "/authenticate",
+//         data: localStorage.getItem("uid=")
+//     })
+//     .then(function(data) {
+//         if (data.email) {
+//             $("#signedIn").show().text("Signed in as " + data.email)
+//             $("#loginDiv").hide()
+//             $("#signupButton").hide()
+//             $("#logout").show()
+//             localStorage.setItem('uid=', data.uid)
+//         }
+//         //Invalid responding with the data provided from the server which is an alert.  Switch to modal in future
+//         else {
+//             alert(data)
+//         }
+//     })
+// }
 
 //Displaying input fields for loging in
 $(document).on('click', "#loginButton", function() {
@@ -46,13 +47,15 @@ $(document).on('click', "#loginButton", function() {
 
 //Ajax call in order for a user to signup.  Passing user email and password.  User will get an email for email authentication
 $(document).on("click", "#signup", function() {
+    console.log("test")
 
     //Grabbing input values and building out our data object
-    let email = $("#username").val()
+    let email = $("#email").val()
     let password = $("#password").val()
     let data = {
         email: email,
-        password: password
+        password: password,
+        username: username
     }
 
     //Post ajax route for our signup
@@ -63,16 +66,17 @@ $(document).on("click", "#signup", function() {
     })
     //promise from server that returns our login info is successful
     .then(function(data) {
+
         console.log(data)
        if (data.email) {
-
             //Change this alert to modal in future on successful login
            alert("Please check your email for a verification link")
+           $("#startModal").modal('toggle')
        } else {
-
             //Change this alert to modal in future on unsuccessful login
             alert("Signup failed")
        }
+
     })
 })
 
