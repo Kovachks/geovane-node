ffa30a8ae3df29d751d4762d0638cabeb3cfe82a
Keith Kovach <kovachks90@gmail.com>
2018-11-12 01:23:44 -0500

    simple data display changes

--- public/js/weatherSearch.js
+++ public/js/weatherSearch.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     })
 
     $("#weatherToggle").on("click", function() {
+        console.log("this is a button test")
         $("#stepDisplayParent").hide()
         $("#weatherDisplayParent").show()
     })
