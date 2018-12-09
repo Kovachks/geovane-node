6d059c9a5e8831fd1c92227288ecd8fbdf4bb1d5
Keith Kovach <kovachks90@gmail.com>
2018-12-09 10:26:47 -0500

    reconfigured autocomplete to remove bad results

--- public/js/autoComplete.js
+++ public/js/autoComplete.js
@@ -1,11 +1,20 @@
-var placesAutocomplete = places({
+
+
+
+const startCityAutocomplete = places({
     appId: 'pl3PTJ4TV68K',
     apiKey: 'dabd71a7698090c4b61f7afde506ff62',
     container: document.querySelector('#startCity')
-  });
+}).configure({
+  countries: ['us'],
+  hitsPerPage: 4,
+});
 
-  var placesAutocomplete = places({
-    appId: 'pl3PTJ4TV68K',
-    apiKey: 'dabd71a7698090c4b61f7afde506ff62',
-    container: document.querySelector('#endCity')
-  });
+const endCityAutocomplete = places({
+  appId: 'pl3PTJ4TV68K',
+  apiKey: 'dabd71a7698090c4b61f7afde506ff62',
+  container: document.querySelector('#endCity')
+}).configure({
+countries: ['us'],
+hitsPerPage: 4,
+});
