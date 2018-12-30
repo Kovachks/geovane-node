db44ec6bd29e245cd290b44d44592879eba9953d
Keith Kovach <kovachks90@gmail.com>
2018-12-29 19:11:47 -0500

    more jquery removed

--- public/js/login.js
+++ public/js/login.js
@@ -7,11 +7,10 @@ document.getElementById('signupButton').addEventListener('click', function() {
 })
 
 document.getElementById('loginButton').addEventListener('click', function() {
-    document.getElementById('modal-content').innerHTML = "<div class='modal-header'><h4 class='modalTitle'>Singup</h4><button type='button' class='close'" +
+    document.getElementById('modal-content').innerHTML = "<div class='modal-header'><h4 class='modalTitle'>Login</h4><button type='button' class='close'" +
     "data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>" + 
-    "<div class='modal-body'><h5>Email</h5>" + 
-    "<input placeholder='Email' type='text' id='email'><h5>Password</h5><input placeholder='Password' type='password' id='password'>" + 
-    "</div><div class='modal-footer'><button type='button' class='btn btn-sm btn-outline-secondary'  id='signup' onclick='login()'>Submit</button><button type='button' class='btn btn-sm btn-outline-secondary' id='cancel'>Cancel</button></div>"
+    "<div class='modal-body'><h5>Email</h5><input placeholder='Email' type='text' id='loginEmail'><h5>Password</h5><input placeholder='Password'" + 
+    "type='password' id='loginPassword'></div><div class='modal-footer'><div class='incorrect'>Password Incorrect</div><button type='button' class='btn btn-sm btn-outline-secondary' id='login' onclick=login()>Login</button><button type='button' class='btn btn-sm btn-outline-secondary' id='cancel'>Cancel</button></div>"
 })
 
 const signup = () => {
@@ -26,7 +25,7 @@ const signup = () => {
 
     request.open('POST', '/signup')
     request.setRequestHeader('Content-Type', 'application/JSON');
-    request.onload = function(data) {
+    request.onload = function() {
         if (request.status = 200) {
             let data = JSON.parse(request.responseText)
             if (data.email) {
@@ -49,8 +48,127 @@ const signup = () => {
 }
 
 const login = () => {
+    let email = document.getElementById('loginEmail').value
+    let password = document.getElementById('loginPassword').value
 
+    let request = new XMLHttpRequest();
+
+    request.open('POST', '/login')
+    request.setRequestHeader('Content-Type', 'application/JSON');
+    request.onload = function() {
+        if (request.status = 200) {
+            let data = JSON.parse(request.response)
+            console.log(data.login)
+            if (data.login === false) {
+                console.log('false')
+                document.getElementsByClassName('incorrect').style.display = 'inline'
+            }
+            else if (data.login === true){
+                console.log('login')
+                document.getElementById('loginDiv').style.visibility = 'hidden'
+                document.getElementById('loginButton').style.visibility = 'hidden'
+                document.getElementById('signupButton').style.visibility = 'hidden'
+                // document.getElementById('logout').style.display = 'inline'
+                $('#startModal').modal('hide')
+
+                sessionStorage.setItem('accessToken=', data.customToken)
+                
+                // Begin authentication
+                let authenticateRequest = new XMLHttpRequest();
+
+                authenticateRequest.open('POST', 'authenticate')
+                authenticateRequest.setRequestHeader('Content-Type', 'application/JSON');
+                authenticateRequest.onload = function() {
+                    if (authenticateRequest.status = 200) {
+                        let authenticateData = JSON.parse(authenticateRequest.response)
+                        console.log(authenticateData)
+                        if (authenticateData.email) {
+                            $("#signedIn").show().text("Signed in as " + authenticateData.email)
+                            $("#signedIn").show();
+                            $("#signedIn").html("<div class='dropdown'><button class='btn btn-sm btn-outline-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
+                               + authenticateData.email + "</button><div class='dropdown-menu' aria-labelledy='dropdownMenuButton'><a class='dropdown-item' href='#'>Account</a><a class='dropdown-item' href='#'>Recent Searches</a><a class='dropdown-item' id='logout' href='#'>Logout</a></div></div>")
+                        }    
+                    }
+                        
+                }
+
+                authenticateRequest.send(JSON.stringify({
+                    accessToken: sessionStorage.getItem("accessToken=")
+                }))
+
+
+            }
+        }
+    }
+    request.send(JSON.stringify({
+        email: email,
+        password: password
+    }))
 }
+
+
+//Click handler for login attempt
+// $(document).on("click", "#login", function() {
+
+//     //Building out our data object with input values from user.
+//     let email = $("#loginEmail").val()
+//     let password = $("#loginPassword").val()
+//     let data = {
+//         email: email,
+//         password: password
+//     }
+
+//     //AJAX post request for login.  Passing data object built above.
+//     $.ajax({
+//         method: "POST",
+//         url: "/login",
+//         data: data
+//     })
+    
+//     //Promise from server for login function.
+//     .then(function(data) {
+//         console.log(data)
+
+//         //Validating if there was a successful email login.  If yes then display users email
+//         if (data.success === 0) {
+           
+//             $(".incorrect").css({'display': 'inline'})
+            
+//         }
+//         //Invalid responding with the data provided from the server which is an alert.  Switch to modal in future
+//         else {
+//             $(".incorrect").css({'display': 'hidden'})
+//             $("#loginDiv").hide()
+//             $("#loginButton").hide()
+//             $("#signupButton").hide()
+//             $("#logout").show()
+
+//             sessionStorage.setItem('accessToken=', data)
+
+//             $("#startModal").modal('hide')
+//                 var data = {
+//                     // uid: sessionStorage.getItem("uid="),
+//                     accessToken: sessionStorage.getItem("accessToken="),
+//                     // apiKey: sessionStorage.getItem("apiKey="),
+//                     // refreshToken: sessionStorage.getItem("refreshToken=")
+//                 }
+//                 $.ajax({
+//                     method: "POST",
+//                     url: "/authenticate",
+//                     data: data
+//                 })
+//                 .then(function(data) {
+//                     if (data.email) {
+// //                        $("#signedIn").show().text("Signed in as " + data.email)
+//                           $("#signedIn").show();
+//                           $("#signedIn").html("<div class='dropdown'><button class='btn btn-sm btn-outline-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
+//                           + data.email + "</button><div class='dropdown-menu' aria-labelledy='dropdownMenuButton'><a class='dropdown-item' href='#'>Account</a><a class='dropdown-item' href='#'>Recent Searches</a><a class='dropdown-item' id='logout' href='#'>Logout</a></div></div>")
+//                     }
+//                 })
+//         }
+//     })
+// })
+
 
 //Logout click handler
 $(document).on("click", "#logout", function() {
@@ -74,68 +192,4 @@ $(document).on("click", "#logout", function() {
 
 $(document).on("click", "#cancel", function() {
     $("#startModal").modal('hide')
-})
-
-//Click handler for login attempt
-$(document).on("click", "#login", function() {
-
-    //Building out our data object with input values from user.
-    let email = $("#loginEmail").val()
-    let password = $("#loginPassword").val()
-    let data = {
-        email: email,
-        password: password
-    }
-
-    //AJAX post request for login.  Passing data object built above.
-    $.ajax({
-        method: "POST",
-        url: "/login",
-        data: data
-    })
-    
-    //Promise from server for login function.
-    .then(function(data) {
-        console.log(data)
-
-        //Validating if there was a successful email login.  If yes then display users email
-        if (data.success === 0) {
-           
-            $(".incorrect").css({'display': 'inline'})
-            
-        }
-        //Invalid responding with the data provided from the server which is an alert.  Switch to modal in future
-        else {
-            $(".incorrect").css({'display': 'hidden'})
-            $("#loginDiv").hide()
-            $("#loginButton").hide()
-            $("#signupButton").hide()
-            $("#logout").show()
-            // localStorage.setItem('uid=', data.uid)
-            // sessionStorage.setItem('uid=', data.uid)
-            sessionStorage.setItem('accessToken=', data)
-            // sessionStorage.setItem('apiKey=', data.stsTokenManager.apiKey)
-            // sessionStorage.setItem('refreshToken=', data.stsTokenManager.refreshToken)
-            $("#startModal").modal('hide')
-                var data = {
-                    // uid: sessionStorage.getItem("uid="),
-                    accessToken: sessionStorage.getItem("accessToken="),
-                    // apiKey: sessionStorage.getItem("apiKey="),
-                    // refreshToken: sessionStorage.getItem("refreshToken=")
-                }
-                $.ajax({
-                    method: "POST",
-                    url: "/authenticate",
-                    data: data
-                })
-                .then(function(data) {
-                    if (data.email) {
-//                        $("#signedIn").show().text("Signed in as " + data.email)
-                          $("#signedIn").show();
-                          $("#signedIn").html("<div class='dropdown'><button class='btn btn-sm btn-outline-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
-                          + data.email + "</button><div class='dropdown-menu' aria-labelledy='dropdownMenuButton'><a class='dropdown-item' href='#'>Account</a><a class='dropdown-item' href='#'>Recent Searches</a><a class='dropdown-item' id='logout' href='#'>Logout</a></div></div>")
-                    }
-                })
-        }
-    })
 })
