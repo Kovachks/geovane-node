e5b2d44627dd1d6a31fb7d1048cf76fabc6cde9a
Keith Kovach <kovachks90@gmail.com>
2019-01-02 19:38:41 -0500

    update to grab from desktop

--- public/js/login.js
+++ public/js/login.js
@@ -62,7 +62,7 @@ const login = () => {
                 document.getElementById('incorrect').style.display = 'inline'
             }
             else if (data.login === true){
-                
+
                 document.getElementById('loginDiv').style.visibility = 'hidden'
                 document.getElementById('loginButton').style.visibility = 'hidden'
                 document.getElementById('signupButton').style.visibility = 'hidden'
@@ -103,71 +103,6 @@ const login = () => {
         password: password
     }))
 }
-
-
-//Click handler for login attempt
-// $(document).on("click", "#login", function() {
-
-//     //Building out our data object with input values from user.
-//     let email = $("#loginEmail").val()
-//     let password = $("#loginPassword").val()
-//     let data = {
-//         email: email,
-//         password: password
-//     }
-
-//     //AJAX post request for login.  Passing data object built above.
-//     $.ajax({
-//         method: "POST",
-//         url: "/login",
-//         data: data
-//     })
-    
-//     //Promise from server for login function.
-//     .then(function(data) {
-//         console.log(data)
-
-//         //Validating if there was a successful email login.  If yes then display users email
-//         if (data.success === 0) {
-           
-//             $(".incorrect").css({'display': 'inline'})
-            
-//         }
-//         //Invalid responding with the data provided from the server which is an alert.  Switch to modal in future
-//         else {
-//             $(".incorrect").css({'display': 'hidden'})
-//             $("#loginDiv").hide()
-//             $("#loginButton").hide()
-//             $("#signupButton").hide()
-//             $("#logout").show()
-
-//             sessionStorage.setItem('accessToken=', data)
-
-//             $("#startModal").modal('hide')
-//                 var data = {
-//                     // uid: sessionStorage.getItem("uid="),
-//                     accessToken: sessionStorage.getItem("accessToken="),
-//                     // apiKey: sessionStorage.getItem("apiKey="),
-//                     // refreshToken: sessionStorage.getItem("refreshToken=")
-//                 }
-//                 $.ajax({
-//                     method: "POST",
-//                     url: "/authenticate",
-//                     data: data
-//                 })
-//                 .then(function(data) {
-//                     if (data.email) {
-// //                        $("#signedIn").show().text("Signed in as " + data.email)
-//                           $("#signedIn").show();
-//                           $("#signedIn").html("<div class='dropdown'><button class='btn btn-sm btn-outline-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
-//                           + data.email + "</button><div class='dropdown-menu' aria-labelledy='dropdownMenuButton'><a class='dropdown-item' href='#'>Account</a><a class='dropdown-item' href='#'>Recent Searches</a><a class='dropdown-item' id='logout' href='#'>Logout</a></div></div>")
-//                     }
-//                 })
-//         }
-//     })
-// })
-
-
 //Logout click handler
 $(document).on("click", "#logout", function() {
     
